30e9c7ac81bedf70734783745d6c793deb21853b
Vraj911 <vraj9112005@gmail.com>
2025-06-06 12:32:44 +0530

    hey

--- server/Server.js
+++ server/Server.js
@@ -1,15 +1,23 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 import connectDB from './configs/Mongodb.js';
-const PORT=process.env.PORT || 4000;
-const app = express();
-await connectDB();
-app.use(express.json());
 
+const app = express();
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send("working"));
 app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.listen(PORT,()=>console.log("server running on PORT "+PORT))
-export default app; 
+
+(async () => {
+  try {
+    await connectDB();
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log("Server running on PORT " + PORT));
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+})();
+
+export default app;
