909ff47d7dbaa61c3f623a525cd3134cd0ec16ef
Vraj911 <vraj9112005@gmail.com>
2025-06-07 17:13:03 +0530

    Final

--- server/Server.js
+++ server/Server.js
@@ -7,18 +7,24 @@ import userRouter from './routes/userRoutes.js';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.get('/', (req, res) => res.send("working"));
-app.use('/api/user',userRouter)
+app.use('/api/user', userRouter);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 (async () => {
   try {
+    console.log('Connecting to MongoDB...');
     await connectDB();
     const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => console.log("Server running on PORT " + PORT));
+    app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
   } catch (error) {
     console.error("Failed to start server:", error);
+    process.exit(1);
   }
 })();
 
