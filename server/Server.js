2928033ce8e45d0ec8a09541e53c8987af5f1521
Vraj911 <vraj9112005@gmail.com>
2025-06-07 16:29:37 +0530

    updateD

--- server/Server.js
+++ server/Server.js
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/Mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send("working"));
+app.use('/api/user',userRouter)
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 (async () => {
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { clerkWebHooks } from '../controllers/userController.js';
+const userRouter = express.Router();
+userRouter.post('/webhooks', clerkWebHooks);
+export default userRouter; 
+
