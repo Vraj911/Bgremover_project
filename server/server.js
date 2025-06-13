f81dbd18286e070189f7b20312fc8bb11de16725
Vraj911 <vraj9112005@gmail.com>
2025-06-14 00:59:54 +0530

    finaly

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/Mongodb.js';
 import userRouter from './routes/userRoutes.js';
-
+import imageRouter from './routes/imageRoutes.js';
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -14,6 +14,7 @@ app.use((err, req, res, next) => {
 
 app.get('/', (req, res) => res.send("working"));
 app.use('/api/user', userRouter);
+app.use('/api/image',imageRouter)
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 (async () => {
